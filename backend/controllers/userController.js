ea02658235f95fbcc825772f2a498f77afb06e6d
Udayx404 <udaygit88@gmail.com>
2026-03-14 12:33:47 +0530

    made some import changes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,5 @@
 import "../config/env.js"
-import userModel from "../models/UserModel.js";
+import userModel from "../models/userModel.js";
 import jwt from "jsonwebtoken"
 import bcrypt from "bcrypt"
 import validator from "validator"
